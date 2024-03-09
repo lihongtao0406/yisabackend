8efb687c5c356d1f895f66fed182d87347d73e95
Tim801 <289011962@qq.com>
2024-03-09 11:18:52 +0800

    'fix'

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
